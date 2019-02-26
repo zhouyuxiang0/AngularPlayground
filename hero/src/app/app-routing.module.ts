e0c3c34df70d973331ec510bf11668cf9ace9d4f
hitagi <1521350289@qq.com>
2019-02-26 15:33:32 +0800

    .

---
+++ hero/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ hero/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Observable, of } from 'rxjs';
+
+import { HEROES } from './mock-heroes';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
