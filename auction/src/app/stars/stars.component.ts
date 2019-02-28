762c4f273e99a79138b8169950438139017f237d
1521350289@qq.com <1521350289@qq.com>
2019-02-28 21:17:53 +0800

    .

---
+++ auction/src/app/stars/stars.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stars',
+  templateUrl: './stars.component.html',
+  styleUrls: ['./stars.component.css']
+})
+export class StarsComponent implements OnInit {
+
+  private stars: boolean[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.stars = [true, true, true, true, true];
+  }
+
+}
